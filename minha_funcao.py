332e760aa51424e64b131396d88d4b347b65e066
Eduardo Sambay <duzeraestudos@gmail.com>
2026-04-27 17:10:18 -0300

    formatar_real

--- minha_funcao.py
+++ minha_funcao.py
@@ -1,25 +1,32 @@
-# geral = "Essa variável posso chamar onde quiser"
-# def  minha_funcao():
-#     print("Essa é minha função")
-#     local = "Essa váriavel só pode ser usada localmente, na função"
-#     true 
+# # geral = "Essa variável posso chamar onde quiser"
+# # def  minha_funcao():
+# #     print("Essa é minha função")
+# #     local = "Essa váriavel só pode ser usada localmente, na função"
+# #     true 
 
-# # chamando a minha função
-# minha_funcao()
-# print(local)
+# # # chamando a minha função
+# # minha_funcao()
 # # print(local)
-# print(geral)
+# # # print(local)
+# # print(geral)
 
-nome = "Alan"  #Global
+# nome = "Alan"  #Global
 
-def saudacao():
-    sobrenome = "Code" # Local
-    print(f"Olá, {nome} {sobrenome}")
+# def saudacao():
+#     sobrenome = "Code" # Local
+#     print(f"Olá, {nome} {sobrenome}")
 
-saudacao()
+# saudacao()
 
-def somar(n1, n2): #n1 e n2 são parâmetros
-    print(f"A soma é {n1 + n2}")
+# def somar(n1, n2): #n1 e n2 são parâmetros
+#     print(f"A soma é {n1 + n2}")
 
-somar(6, 40) #6 e 40 são argumentos
+# somar(6, 40) #6 e 40 são argumentos
 
+
+def formatar_real(valor):
+    return f"R$ {valor:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+
+# Uso:
+preco_hospedagem = 49.9
+print(formatar_real(preco_hospedagem)) # R$ 49,90
