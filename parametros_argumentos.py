dba2234be645f558a1fa50283fa9bfb4291f06f9
Eduardo Sambay <duzeraestudos@gmail.com>
2026-04-28 16:39:07 -0300

    lista

--- parametros_argumentos.py
+++ parametros_argumentos.py
@@ -12,16 +12,31 @@
 # criar_usuario(idade=30, nome="Carol")
 # criar_usuario("Dan", 25, admin=True)
 
-def criar_perfil(nome, idade, cidade):
-    print(f'{nome}, {idade} anos, {cidade}')
+# def criar_perfil(nome, idade, cidade):
+#     print(f'{nome}, {idade} anos, {cidade}')
 
-criar_perfil(cidade="Curitiba", nome="Julia", idade=25) 
-#Julia, 25 anos, Curitiba - funciona independente da ordem 
+# criar_perfil(cidade="Curitiba", nome="Julia", idade=25) 
+# #Julia, 25 anos, Curitiba - funciona independente da ordem 
 
 
-def somar_tudo(*numeros):
-    return sum(numeros)
+# def somar_tudo(*numeros):
+#     return sum(numeros)
 
-print(somar_tudo(1, 2))
-print(somar_tudo( 1, 2, 3, 4))
-print(somar_tudo(20, 30))
+# print(somar_tudo(1, 2))
+# print(somar_tudo( 1, 2, 3, 4))
+# print(somar_tudo(20, 30))
+
+#Criando um dicionário
+
+produto = {"nome": "Teclado", "preco": 150, "em_estoque": True}
+
+# Acessando um valor 
+print(produto["nome"])
+
+# Modificando um valor
+produto["preco"] = 135.00
+
+# Adicionando um novo par
+produto["quantidade"] = 10
+
+print(produto)
